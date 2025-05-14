5f6ec0b6caf215357f773262d989ed37b3d0bc14
RuponGomeZ <anthonyrupon0@gmail.com>
2025-05-14 21:55:52 +0600

    tanstack querry added

--- src/pages/AddService.jsx
+++ src/pages/AddService.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 
 const AddService = () => {
+
+
+
     return (
         <div>
-            <input
-                type="text"
-                placeholder="Type here"
-                className="input input-bordered input-md w-full max-w-xs" />
+            <div className="card bg-base-100 mx-auto mt-14 w-full max-w-sm shrink-0 shadow-2xl">
+                <div className="card-body"></div>
+                <form className="fieldset">
+
+                    {/* Image Url */}
+                    <label className="label">Image Url</label>
+                    <input name='imageUrl' type="url" className="input" placeholder="Image url here" />
+
+                    {/* Service Name */}
+                    <label className="label">Service Name</label>
+                    <input name='name' type="text" className="input" placeholder="Service Name" />
+
+                    {/* Price */}
+                    <label className="label">Price</label>
+                    <input name='price' type="number" className="input" placeholder="Enter Your desired price" />
+
+                    {/* Service Area */}
+                    <label className="label">Service Area</label>
+                    <input name='area' type="text" className="input" placeholder="Service Area" />
+
+                    {/* Description */}
+                    <div><a className="link link-hover">Forgot password?</a></div>
+                    <button className="btn btn-neutral mt-4">Add your service</button>
+                </form>
+            </div>
         </div>
     );
 };
